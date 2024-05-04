2fb114f8efb425ff03383d4b1accd074075878ae
Aldi <alfahluzi@gmail.com>
2024-05-04 19:21:33 +0700

    simplify conf

--- api/routes.py
+++ api/routes.py
@@ -37,13 +37,7 @@ rest_api = Api(version="1.0", title="Users API")
 
 summarize_model = rest_api.model('Summarize', {"text": fields.String(required=True, min_length=2, max_length=512),
                                                 "min_length": fields.Integer(required=False, min=1, max=256),
-                                                "num_beams": fields.Integer(required=False, min=1),
-                                                "repetition_penalty": fields.Float(required=False, min=1.0),
-                                                "length_penalty": fields.Float(required=False, min=1.0),
-                                                "no_repeat_ngram_size": fields.Float(required=False, min=1.0),
                                                 "temperature": fields.Float(required=False, min=0.01),
-                                                "top_k": fields.Float(required=False, min=0, max=100),
-                                                "top_p": fields.Float(required=False, min=0, max=1),
                                               })
 
 
@@ -110,17 +104,8 @@ class Summarize(Resource):
         summary_ids = model.generate(
                     input_ids.to(device),
                     max_length = 256, 
-                    early_stopping = True,
-                    use_cache = True,
-                    do_sample = True,
                     min_length = req_data.get("min_length") or 10,
-                    num_beams = req_data.get("num_beams") or 10,
-                    repetition_penalty = req_data.get("repetition_penalty") or 2.0,
-                    length_penalty = req_data.get("length_penalty") or 2.0,
-                    no_repeat_ngram_size = req_data.get("no_repeat_ngram_size") or 3,
                     temperature = req_data.get("temperature") or 1.0,
-                    top_k = req_data.get("top_k") or 50,
-                    top_p = req_data.get("top_p") or 0.95,
         )
 
         summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
